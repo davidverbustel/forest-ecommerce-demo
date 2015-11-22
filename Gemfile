e7f840bfbaa32e3c07bebe0a1841762c065b2ead
David Verbustel <david.verbustel@gmail.com>
2015-11-22 19:46:24 +0100

    added items and seeds

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'figaro'
-gem 'faker', group: :production
+gem 'faker'
 ruby '2.2.2'
 gem 'rails_12factor', group: :production
 gem 'puma',           group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121112131) do
+ActiveRecord::Schema.define(version: 20151122151917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20151121112131) do
     t.date     "birthday"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "currency"
+    t.float    "price"
+    t.string   "category"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
